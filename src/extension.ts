f9fe4f31335e6f32bb70e006181111ece9d598a6
tog2sensei <sisbori573@gmail.com>
2022-07-10 02:26:10 +0900

    add return somethings

--- src/extension.ts
+++ src/extension.ts
@@ -11,6 +11,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-breaktime" is now active!');
 
+	let charcount = new CharCount();
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -20,21 +22,9 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from vscode-breaktime!',{
 			modal: true
 		});
+
+		charcount.updateCount();
 	});
-
-	/*
-	let editor = vscode.window.activeTextEditor; // 現在開いているエディタ
-	let document = editor?.document; // ドキュメント
-
-	let linePrev = 0; // ドキュメントの変更前の行数
-	let lineNow = document?.lineCount;
-	let line = 0; // 行数を格納する変数
-
-	line = lineNow - linePrev;
-	*/
-
-
-
 
 	context.subscriptions.push(disposable);
 }
@@ -50,6 +40,11 @@ export class CharCount{
 	private _assenblyLineNum: Array<number> = []; // 各ファイルごとの行数
 	private _assenblyFileName: Array<String> = []; // 各ファイルの名前
 
+	/**	public
+		エディタ情報を更新するメソッド
+		引数 なし;
+		返り値 なし
+	 **/
 	public updateCount(){
 		let editor = vscode.window.activeTextEditor; // 現在開いているエディタ情報を格納
 		if(!editor){ // "editor"がundefinedである場合
@@ -76,15 +71,17 @@ export class CharCount{
 			this._nowFileNum = this._assenblyFileName.length - 1; // 現在のファイルを更新
 		}
 
-		console.log(this._fileName + "..." + "charactors:" + this._stringNum + " lines:" + this._lineNum); // コンソールに現在のファイルの情報を追加
+		console.log(this._fileName + " ..." + "charactors:" + this._stringNum + " lines:" + this._lineNum); // コンソールに現在のファイルの情報を追加
+		console.log("sum..." + " charactors: " + this.returnStrNum());
 
 	}
 
-	/* 引数であるドキュメントの文字数を返すメソッド
-		引数 document (vscode.TextDocument型)
+	/** private
+		引数であるドキュメントの文字数を返すメソッド
+		引数 document (vscode.TextDocument型);
 		返り値 strNum (number型) ... 文字数
-	*/
-	public getNumChar(document: vscode.TextDocument): number {
+	**/
+	private getNumChar(document: vscode.TextDocument): number {
 		let strDoc = document.getText(); // ドキュメントの文字をString型で所得
 		let strNum : number = 0; // 文字数を格納するnumber型の変数
 
@@ -97,12 +94,12 @@ export class CharCount{
 		return strNum;
 	}
 
-	/*
+	/** public
 		引数にファイル名が格納されていれば，そのファイルの文字数を，
 		格納されて居なければ全てのファイルの合計文字数を返す
-		引数 name(String型)...ファイル名
+		引数 name(String型)...ファイル名;
 		返り値 number(number型)...文字数
-	*/
+	**/
 	public returnStrNum(name?: String): number{
 		let number = 0; // 返り値用
 
@@ -128,12 +125,51 @@ export class CharCount{
 		return number;
 	}
 
-	/*
+	/** public
+	 *	引数にファイル名が格納されていれば，そのファイルの行数を，
+	 *	格納されて居なければ全てのファイルの合計行数を返す.
+	 *	引数 name(String型)...ファイル名;
+	 *	返り値 number(number型)...文字数
+	**/
+	public returnLineNum(name?: String){
+		let number = 0; // 返り値用
+
+		if(!name){ // 引数の記述がない場合
+			let sum = 0; // 文字数の合計
+
+			// 全ての文字数を求める
+			for(let i = 0; i < this._assenblyFileName.length; i++){
+				sum += this._assenblyLineNum[i]; // i の時のassenblyStrNumの数を足す
+			}
+
+			number = sum; // 返り値用の変数に合計を格納
+		}
+		else{
+			if(this.searchFileName(name)){
+				number = this._assenblyLineNum[this._nowFileNum];
+			}
+			else{
+				number = 0;
+			}
+		}
+
+		return number;
+	}
+
+	/**
+	 * ファイル名をまとめて返すメソッド
+	 * 返り値 (String[]型)
+	 */
+	public returnFileName(): String[]{
+		return this._assenblyFileName;
+	}
+
+	/**	private
 		引数のファイル名と同じファイル名が格納されているかを確認し格納されていればtrue
 		格納されていなければfalseを返すメソッド
-		引数 name(String型) ... ファイル名
+		引数 name(String型) ... ファイル名;
 		返り値 (boolean型)
-	*/
+	**/
 	private searchFileName(name: String): boolean{
 		for(let i = 0; i < this._assenblyFileName.length; i++){
 			// iの時の_assenblyFileNameとnameが一致する時
@@ -147,8 +183,5 @@ export class CharCount{
 	}
 }
 
-class CharCountController{
-
-}
 // this method is called when your extension is deactivated
 export function deactivate() {}
