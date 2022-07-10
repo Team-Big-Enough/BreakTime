4bdaf178efd6340b49e4b508a006fc2a77e7d0b7
maple-fm <fuma0205boo@gmail.com>
2022-07-10 14:31:58 +0900

    休憩ごとにgifとグラフの表示

--- src/extension.ts
+++ src/extension.ts
@@ -29,37 +29,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('休憩してください！', {
-			modal: true,
-		});
 
-		setTimeout(startbreak, INTERVAL);
+		setTimeout(startbreak, INTERVAL, context);
 
 	});
-
-	
-	// const kyuukeiFigures = {'休憩': 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEixzkaQI4759eBna_CffZH0DkXV7w1cJV_QPcYJMRO8X5ZsLnXrqHJi3QxQFsrYY85aPCnunvHebaBS2hUayLfcSLPiz0GJBjcO7nwWq019lDW4qCDg6d3fCy7k4dHghyM3mTBO_XXn0ExyduUJu537k187TDICM1a-e1RckDLmv6BEjab9g_3SbaLQ/s640/1204639_s%20(1).jpg'};
-	const kyuukeiFigures = {'休憩': 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEibNNjxJIu-0NU_bkVjslf6-CN7u6VGUUQsst4_-_PhGbaASpwuoDsF6fvtliWir7rfrB45XGZHdEbVCAp1utUWG7dhfWDp2-DG_r3-s0agCs5srD2qqRjaQdYXYE-iBd2BGloB_J62bjZYJ0pGdIAQsyMMNTCbJtaqVeUYtwfxB1SmxoNB-qQMQSGp/s1000/11792.gif'};
-
-
-	
-
-	function getWebviewContent(kyuukeiResult: keyof typeof kyuukeiFigures) {
-		return `<!DOCTYPE html>
-		<html lang="ja">
-		<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		</head>
-		<body>
-		<img src="${kyuukeiFigures[kyuukeiResult]}" />
-		</body>
-		</html>`;
-	}
-	const kyuukeiCandidates = ['休憩'] as const;
-	const kyuukeiResult = kyuukeiCandidates[Math.floor(Math.random() * kyuukeiCandidates.length)];
-	const panel = vscode.window.createWebviewPanel('breaktime',`お疲れ様です。${kyuukeiResult}のお時間です！`,vscode.ViewColumn.One,{});
-	panel.webview.html = getWebviewContent(kyuukeiResult);
 
 	context.subscriptions.push(disposable);
 
@@ -68,59 +41,10 @@ export function activate(context: vscode.ExtensionContext) {
 	//context.subscriptions.push(disposable);
 	context.subscriptions.push(countEventCont);
 
-
-	// webviewはローカルリソースに直接アクセスできないらしい
-	// 読み込みたいときはWebview.asWebviewUri関数を使って、読み込める形に変換しないといけない
-	// https://code.visualstudio.com/api/extension-guides/webview#loading-local-content
-	// Create and show a new webview
-	const panell = vscode.window.createWebviewPanel(
-		'Graph', // Identifies the type of the webview. Used internally
-		'test area', // Title of the panel displayed to the user
-		vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-		{enableScripts: true} // Webview options. More on these later.
-	);
-
-	const graphPath = vscode.Uri.file(
-		path.join(context.extensionPath, 'src', 'graph.js')
-	);
-	const graphSrc = panell.webview.asWebviewUri(graphPath);
-
-	panell.webview.html = getWebviewContents(graphSrc);
-
-	// context.subscriptions.push(disposable);	
-
-	function getWebviewContents(graphSrc: vscode.Uri){
-		return `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>title</title>
-		</head>
-		<body>
-			<h1>title</h1>
-	
-			<div class=""><input type="radio" name="analysis" value="bar" checked>縦棒グラフ</div>
-			<div class=""><input type="radio" name="analysis" value="Line">折れ線グラフ</div>
-			<div class=""><input type="radio" name="analysis" value="3">散布図</div>
-	
-			<div>
-				<canvas id="graph" width="100%"></canvas>
-			</div>
-	
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
-			<script src=` + graphSrc + `></script>
-		</body>
-		</html>
-		`;
-	}
-	
-	function showModalMessage(msg: string){
-			vscode.window.showInformationMessage(msg, {modal: true});
-	}
-
 }
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
 
 // 文字をカウントするクラス
 export class CharCount{
@@ -307,24 +231,60 @@ class CountEventControler{
 }
 
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
-
 /*
-タイマーをセットする
+*タイマーをセットする
 */
-function startbreak(){
+function startbreak(context: vscode.ExtensionContext){
 	vscode.window.showInformationMessage('休憩してください！', {
 		modal: true,
 	});
 
-	timer(MINITES,SECONDS);// 分：秒
+	const kyuukeiFigures = {'休憩': 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEibNNjxJIu-0NU_bkVjslf6-CN7u6VGUUQsst4_-_PhGbaASpwuoDsF6fvtliWir7rfrB45XGZHdEbVCAp1utUWG7dhfWDp2-DG_r3-s0agCs5srD2qqRjaQdYXYE-iBd2BGloB_J62bjZYJ0pGdIAQsyMMNTCbJtaqVeUYtwfxB1SmxoNB-qQMQSGp/s1000/11792.gif'};
+
+	function getWebviewContent(kyuukeiResult: keyof typeof kyuukeiFigures) {
+		return `<!DOCTYPE html>
+		<html lang="ja">
+		<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		</head>
+		<body>
+		<img src="${kyuukeiFigures[kyuukeiResult]}" />
+		</body>
+		</html>`;
+	}
+	const kyuukeiCandidates = ['休憩'] as const;
+	const kyuukeiResult = kyuukeiCandidates[Math.floor(Math.random() * kyuukeiCandidates.length)];
+	const panel = vscode.window.createWebviewPanel('breaktime',`お疲れ様です。${kyuukeiResult}のお時間です！`,vscode.ViewColumn.One,{});
+	panel.webview.html = getWebviewContent(kyuukeiResult);
+
+	// webviewはローカルリソースに直接アクセスできないらしい
+	// 読み込みたいときはWebview.asWebviewUri関数を使って、読み込める形に変換しないといけない
+	// https://code.visualstudio.com/api/extension-guides/webview#loading-local-content
+	// Create and show a new webview
+	const panell = vscode.window.createWebviewPanel(
+		'Graph', // Identifies the type of the webview. Used internally
+		'test area', // Title of the panel displayed to the user
+		vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+		{enableScripts: true} // Webview options. More on these later.
+	);
+
+	const graphPath = vscode.Uri.file(
+		path.join(context.extensionPath, 'src', 'graph.js')
+	);
+	const graphSrc = panell.webview.asWebviewUri(graphPath);
+
+	panell.webview.html = getWebviewContents(graphSrc);
+
+
+
+	timer(MINITES,SECONDS, context);// 分：秒
 }
 
 /*
-タイマー開始
+*タイマー開始
 */
-function timer(min: number, sec: number){
+function timer(min: number, sec: number, context: vscode.ExtensionContext){
 
 	if(sec - 1 === -1){
 		min --;
@@ -336,7 +296,7 @@ function timer(min: number, sec: number){
 	console.log(min, sec);
 	// vscode.window.showInformationMessage("残り"+min+ "分"+sec+"秒です!");
 	vscode.window.setStatusBarMessage("残り"+min+ "分"+sec+"秒です!", min*60000+sec*1000);
-	var id = setTimeout(timer, 1000, min, sec);
+	var id = setTimeout(timer, 1000, min, sec, context);
 	// var id = setInterval(timer, 2000, min, sec);
 
 	// 終了
@@ -345,7 +305,34 @@ function timer(min: number, sec: number){
 		vscode.window.showInformationMessage('休憩終了です！', {
 			modal: true,
 		});
-		setTimeout(startbreak, INTERVAL);
+		setTimeout(startbreak, INTERVAL, context);
 	}
 
+}
+
+function getWebviewContents(graphSrc: vscode.Uri){
+	return `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>title</title>
+	</head>
+	<body>
+		<h1>title</h1>
+
+		<div class=""><input type="radio" name="analysis" value="bar" checked>縦棒グラフ</div>
+		<div class=""><input type="radio" name="analysis" value="Line">折れ線グラフ</div>
+		<div class=""><input type="radio" name="analysis" value="3">散布図</div>
+
+		<div>
+			<canvas id="graph" width="100%"></canvas>
+		</div>
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+		<script src=` + graphSrc + `></script>
+	</body>
+	</html>
+	`;
 }
