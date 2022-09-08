c037f87c2d67c08086fc72d26f7af70b934b146e
さくや <shuoyeshantian@gmail.com>
2022-09-08 18:20:40 +0900

    タイマー作成

--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-
-import { countReset } from 'console';
-import * as typescript from 'typescript';
 import * as vscode from 'vscode';
-import * as path from 'path';
 import count = require('./count/count'); // count.tsにある文字数カウントクラスなどをインポート
 import globalData = require("./count/controlData");
 
-const MINITES = 0; // m
-const SECONDS = 30; // s
-const INTERVAL = 30000; // ms : 30秒
-
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const MINITES = 0;
+const SECONDS = 2;
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-breaktime" is now active!');
 
 	let charCount = new count.CharCount();
 	let countEventCont = new count.CountEventController(charCount);
-
 	let data = new globalData.Data(context);
-	
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-
-	let disposable = vscode.commands.registerCommand('vscode-breaktime.helloWorld', () => {
-
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-
-		setTimeout(startbreak, INTERVAL, context, data, charCount);
-
-
-	});
-
-
-	context.subscriptions.push(disposable);
-
+	setTimer(MINITES, SECONDS, true);
 
 	// リソース解放
-	//context.subscriptions.push(disposable);
 	context.subscriptions.push(countEventCont);
 
 }
@@ -56,91 +24,117 @@ export function deactivate() {
 
 }
 
+/**
+ * ランダムに配列の中身を一つ返す
+ * @param {Array<string>} array string型の配列
+ * @return {string} 配列の中身
+ */
+ function getRandomMessage(array: Array<string>): string{
+	return array[Math.floor(Math.random() * array.length)];
+}
 
+/**
+ * 書式の桁数に満たない数値の場合に、0を追加して桁数を合わせる。
+ * @param {number} num 値
+ * @param {number} len 桁数
+ * @returns {string} 指定された桁数に整形した文字列
+ */
+function zeroPadding(num: number, len: number): string{
+	return ( Array(len).join('0') + num ).slice( -len );
+}
 
-/*
-*タイマーをセットする
-*/
-function startbreak(context: vscode.ExtensionContext, input: globalData.Data, content: count.CharCount){
-	vscode.window.showInformationMessage('休憩してください！', {
-		modal: true,
+/**
+ * タイマー開始
+ * @param {number} min 分数
+ * @param {number} sec 秒数
+ * @param {boolean | undefined} stateFlag 状態管理フラグ(true: 作業中, false: 休憩中)
+ */
+function setTimer(min: number, sec: number, stateFlag: boolean){
+	const text = stateFlag
+		? '残り作業時間 '
+		: '残り休憩時間 ';
+
+	const id = setInterval(function(){
+		vscode.window.setStatusBarMessage(
+			text+zeroPadding(min, 2)+':'+zeroPadding(sec--, 2));
+		if(sec === -1){
+			min--;
+			sec = 59;
+		}
+
+		min === -1? clearTimer(id, stateFlag): '';
+	}, 1000);
+}
+
+/**
+ * タイマー終了 
+ * @param {NodeJS.Timer} id タイマーID
+ * @param {boolean} stateFlag 状態管理フラグ（）
+ */
+function clearTimer(id: NodeJS.Timer, stateFlag: boolean){	//, input: globalData.Data, content: count.CharCount
+	// メッセージを考える
+	const breakMessage: Array<string> = [
+		'休憩してください。1',
+		'休憩してください。2',
+		'休憩してください。3',
+		'休憩してください。4',
+	];
+	const workMessage: Array<string> = [
+		'作業を開始してください。1',
+		'作業を開始してください。2',
+		'作業を開始してください。3',
+		'作業を開始してください。4',
+	];
+	const message: string = stateFlag
+		? getRandomMessage(breakMessage)
+		: getRandomMessage(workMessage);
+
+	clearInterval(id);
+	const window = vscode.window.showInformationMessage(
+		message, { modal: true },
+		{ title: 'いいえ', isCloseAffordance: false },
+		{ title: 'はい', isCloseAffordance: true }
+	);
+
+	window.then((value) => {
+		if(stateFlag){ 
+            if(value?.isCloseAffordance){
+                setTimer(MINITES, SECONDS, false);      // 休憩する
+
+                // input.dataInput(content);   // データをglobalStorageに格納する
+											   // 要相談(構造的にsetTimerの引数にもinput, contentが必要になる	
+											   // 		 都度インスタンスの生成でもいい？)
+
+                // ビューの表示
+            }else{ setTimer(MINITES, SECONDS, true); }  // 休憩せずに作業を続ける
+        }else{ 
+            if(value?.isCloseAffordance){
+                setTimer(MINITES, SECONDS, true);       // 作業する
+            }else{
+                setTimer(MINITES, SECONDS, false);      // 作業せずに休憩を続ける
+                // ビューの表示
+            }
+        }
 	});
-
-	input.dataInput(content); // データをglobalStorageに格納する
-	
-	console.log(input.returnNumOfString());
-	
-
-	const kyuukeiFigures = {'休憩': 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEibNNjxJIu-0NU_bkVjslf6-CN7u6VGUUQsst4_-_PhGbaASpwuoDsF6fvtliWir7rfrB45XGZHdEbVCAp1utUWG7dhfWDp2-DG_r3-s0agCs5srD2qqRjaQdYXYE-iBd2BGloB_J62bjZYJ0pGdIAQsyMMNTCbJtaqVeUYtwfxB1SmxoNB-qQMQSGp/s1000/11792.gif'};
-
-	function getWebviewContent(kyuukeiResult: keyof typeof kyuukeiFigures) {
-		return `<!DOCTYPE html>
-		<html lang="ja">
-		<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		</head>
-		<body>
-		<img src="${kyuukeiFigures[kyuukeiResult]}" />
-		</body>
-		</html>`;
-	}
-	const kyuukeiCandidates = ['休憩'] as const;
-	const kyuukeiResult = kyuukeiCandidates[Math.floor(Math.random() * kyuukeiCandidates.length)];
-	const panel = vscode.window.createWebviewPanel('breaktime',`お疲れ様です。${kyuukeiResult}のお時間です！`,vscode.ViewColumn.One,{});
-	panel.webview.html = getWebviewContent(kyuukeiResult);
-
+}
 	// webviewはローカルリソースに直接アクセスできないらしい
 	// 読み込みたいときはWebview.asWebviewUri関数を使って、読み込める形に変換しないといけない
 	// https://code.visualstudio.com/api/extension-guides/webview#loading-local-content
 	// Create and show a new webview
-	const panell = vscode.window.createWebviewPanel(
-		'Graph', // Identifies the type of the webview. Used internally
-		'test area', // Title of the panel displayed to the user
-		vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-		{enableScripts: true} // Webview options. More on these later.
-	);
+	// const panell = vscode.window.createWebviewPanel(
+	// 	'Graph', // Identifies the type of the webview. Used internally
+	// 	'test area', // Title of the panel displayed to the user
+	// 	vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+	// 	{enableScripts: true} // Webview options. More on these later.
+	// );
 
-	const graphPath = vscode.Uri.file(
-		path.join(context.extensionPath, 'src', 'graph.js')
-	);
-	const graphSrc = panell.webview.asWebviewUri(graphPath);
+	// const graphPath = vscode.Uri.file(
+	// 	path.join(context.extensionPath, 'src', 'graph.js')
+	// );
+	// const graphSrc = panell.webview.asWebviewUri(graphPath);
 
-	panell.webview.html = getWebviewContents(graphSrc);
+	// panell.webview.html = getWebviewContents(graphSrc);
 
-
-
-	timer(MINITES,SECONDS, context, input, content);// 分：秒
-}
-
-/*
-*タイマー開始
-*/
-function timer(min: number, sec: number, context: vscode.ExtensionContext, input: globalData.Data, content: count.CharCount){
-
-	if(sec - 1 === -1){
-		min --;
-		sec = 59;
-	}
-	else{
-		sec --;
-	}
-	//console.log(min, sec);
-	// vscode.window.showInformationMessage("残り"+min+ "分"+sec+"秒です!");
-	vscode.window.setStatusBarMessage("残り"+min+ "分"+sec+"秒です!", min*60000+sec*1000);
-	var id = setTimeout(timer, 1000, min, sec, context, input, content);
-	// var id = setInterval(timer, 2000, min, sec);
-
-	// 終了
-	if(min < 0 ){
-		clearTimeout(id);
-		vscode.window.showInformationMessage('休憩終了です！', {
-			modal: true,
-		});
-		setTimeout(startbreak, INTERVAL, context, input, content);
-	}
-
-}
 
 function getWebviewContents(graphSrc: vscode.Uri){
 	return `
